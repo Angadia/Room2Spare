88e26ec5ad9e8a219d90bbd2ad2f5560cafb4add
Aline Ribeiro <alinear.mail@gmail.com>
2020-02-01 19:13:01 -0800

    Create a migration to change user type value default to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_221347) do
+ActiveRecord::Schema.define(version: 2020_02_02_030018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,10 +120,10 @@ ActiveRecord::Schema.define(version: 2020_02_01_221347) do
     t.string "last_name"
     t.string "email"
     t.string "contact_number"
-    t.boolean "is_admin"
-    t.boolean "is_manager"
-    t.boolean "is_teacher"
-    t.boolean "is_student"
+    t.boolean "is_admin", default: false
+    t.boolean "is_manager", default: false
+    t.boolean "is_teacher", default: false
+    t.boolean "is_student", default: false
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
