8a6f3be1ad679a1844033281d50f98e6dafebe2d
Aline Ribeiro <alinear.mail@gmail.com>
2020-02-01 17:45:25 -0800

    Created a welcome controller, route and view just to test login and sign up redirection

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+    # I created this controller just to test if sign in is working
+    def index
+    end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
   resource  :session, only: [:new, :create, :destroy]
+
+  # I created this route only to test sign in redirection
+  get('/', {to: 'welcome#index', as: 'root'})
+
 end
