efdaecb1f8b42393eb98cb4934b2348754332d28
Aline Ribeiro <alinear.mail@gmail.com>
2020-02-01 17:11:26 -0800

    Created private methods user_signed_in?, current_user, authenticate_user on the application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
       @user = User.new user_params
       if @user.save
+          session[:user_id] = @user.id
           redirect to root_path  #root_path nao existe ainda
       else
           render :new
