614558bd5af6ffe96f0227fc91802b28ca942a01
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-02-02 12:53:31 -0800

    commit for rebase

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,8 @@
 class RoomsController < ApplicationController
+    before_action :authenticate_user!, except: [:show, :index]
+    before_action :find_room, only: [:show, :edit, :update, :destroy]
+    before_action :authorize!, only: [:edit, :update, :destroy]
+
     
     def new
         @room = Room.new
@@ -6,6 +10,10 @@ class RoomsController < ApplicationController
 
     def index
         @rooms= Room.order(created_at: :DESC)
+    end
+
+    def show
+        @availability = Availability.new
     end
 
     def create
@@ -28,6 +36,11 @@ class RoomsController < ApplicationController
     end
     
     def room_params
-        params.require(:room).permit(:capicity, :area, :whiteboard, :internet, :name)
+        params.require(:room).permit(:capacity, :area, :whiteboard, :internet, :name)
     end
+
+    def authorize!
+        redirect_to room_path(@room), alert: "access denied" unless can? :crud, @room
+    end
+
 end
