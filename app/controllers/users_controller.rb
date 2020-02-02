951dcb80d367769f7ddb0943d299da0a76c716b6
Aline Ribeiro <alinear.mail@gmail.com>
2020-02-01 20:06:50 -0800

    Changed create user action to allow creating of different types of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,14 @@ class UsersController < ApplicationController
 
   def create
       @user = User.new user_params
+      if params[:is_manager] == "manager"
+        @user.is_manager = true
+      elsif params[:is_teacher] == "teacher"
+        @user.is_teacher = true
+      elsif params[:is_student] == "student"
+        @student.is_student = true
+      end
+      
       if @user.save
           session[:user_id] = @user.id
           redirect_to root_path  
