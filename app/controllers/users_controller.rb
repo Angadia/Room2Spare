045d83f52e1b4f378b7b8b57e68b2885e9a70192
Aline Ribeiro <alinear.mail@gmail.com>
2020-02-02 16:31:09 -0800

    Cleaned code on the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,28 +6,30 @@ class UsersController < ApplicationController
       @user = User.new
   end
 
-  def create
-      @user = User.new user_params
-      if params[:user_type] == "manager"
-        @user.is_manager = true
-      elsif params[:user_type] == "teacher"
-        @user.is_teacher = true
-      elsif params[:user_type] == "student"
-        @user.is_student = true
-      end
-      if @user.save
-          session[:user_id] = @user.id
-          if @user.is_admin == true || @user.is_manager == true
-           redirect_to facilities_path   #this is not the final redicrection, just test, change home link in application erb
-          elsif @user.is_teacher
-            redirect_to courses_path
-          else
-            redirect_to courses_path    #this is not the final redicrection, just test
-          end
-      else
-          render :new
+
+def create
+  @user = User.new user_params
+  if params[:user_type] == "manager"
+    @user.is_manager = true
+  elsif params[:user_type] == "teacher"
+    @user.is_teacher = true
+  elsif params[:user_type] == "student"
+    @user.is_student = true
   end
+  if @user.save
+      session[:user_id] = @user.id
+      if @user.is_admin  || @user.is_manager 
+       redirect_to facilities_path   #this is not the final redicrection, just test
+      elsif @user.is_teacher
+        redirect_to courses_path
+      else
+        redirect_to courses_path    #this is not the final redicrection, just test
+      end
+  else
+      render :new
 end
+end
+
 
 
 def edit
@@ -43,7 +45,7 @@ def update
   if can? :crud, @user
     if @user.update user_params
         flash[:notice] = 'User updated Successfully'
-        if @user.is_admin == true || @user.is_manager == true
+        if @user.is_admin || @user.is_manager
           redirect_to facilities_path   #this is not the final redicrection, just test, change home link in application erb
          elsif @user.is_teacher
            redirect_to courses_path
